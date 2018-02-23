3c984c0b79d0ddf5a06bbd34d54b3e8b3140d4a2
hakman00 <kleinh001@gmail.com>
2018-02-23 12:29:01 +0200

    Update addresses developer notes eg name , stud num etc.

--- src/com/IntelliJProjects/AdditionApp.java
+++ src/com/IntelliJProjects/AdditionApp.java
@@ -1,3 +1,10 @@
+/*
+    NAME            :   Heinrich Klein
+    STUDENT NUMBER  :   195032659
+    DATE            :   23 Feb 2018
+    DESCRIPTION     :   This project was cloned from a peers github,
+                        then modified and pushed to my github repository.
+ */
 package com.IntelliJProjects;
 
 import javax.swing.*;
@@ -6,13 +13,13 @@ public class AdditionApp {
 
     public static void main(String[] args) {
 	// write your code here
-        String s1 = JOptionPane.showInputDialog("Please enter 1st Number: ");
-        String s2 = JOptionPane.showInputDialog("Please enter 2nd Number: ");
+        String s1 = JOptionPane.showInputDialog("ENTER FIRST NUMBER: "); //upadted for user readability
+        String s2 = JOptionPane.showInputDialog("ENTER SECOND NUMBER: "); //upadted for user readability
         int num1 = Integer.parseInt(s1);
         int num2 = Integer.parseInt(s2);
 
         int sum = num1 + num2;
 
-        JOptionPane.showMessageDialog(null,"The answer is :"+sum,"Results", JOptionPane.INFORMATION_MESSAGE);
+        JOptionPane.showMessageDialog(null,"ADDITION RESULT: "+sum,"Results", JOptionPane.INFORMATION_MESSAGE); //upadted for user readability
     }
 }
