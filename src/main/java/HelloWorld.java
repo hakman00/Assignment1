18f0811d8f53feb66931578180e6f9e1c0014101
hakman00 <kleinh001@gmail.com>
2018-02-23 14:46:06 +0200

    Update to address a friendly message to user.

--- src/main/java/HelloWorld.java
+++ src/main/java/HelloWorld.java
@@ -1,7 +1,17 @@
+/*
+    NAME            :   Heinrich Klein
+    STUDENT NUMBER  :   195032659
+    Date            :   23 Feb 2018
+    DESCRIPTION     :   Clone a peers repository, modify the code
+                        then push to a different branch in my github repository.
+*/
+
 public class HelloWorld
 {
+
     public static void main (String[]args)
     {
-        System.out.println("Hellow World");
+        //changing text to make more user friendly
+        System.out.println("Hi Mr Anikwue, slowly but surely i'm begining to understand GiT...");
     }
 }
